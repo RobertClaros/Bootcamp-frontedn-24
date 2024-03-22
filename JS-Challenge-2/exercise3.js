f363d6d6731892bb5e63438193d987cebec7624d
RobertClaros <audioprobolivia@gmail.com>
2024-03-22 05:37:02 -0400

    cleanup package three for JS-challenge-3

--- JS-Challenge-2/exercise3.js
+++
@@ -1,8 +0,0 @@
-let arr2 = [7, 9, 1, 'a', 'a', 'f', 9, 4, 2, 'd', 'd'];
-
-function removeDuplicates(arr) {
-    return [...new Set(arr)];
-}
-
-let uniqueArr = removeDuplicates(arr2);
-console.log(uniqueArr);
